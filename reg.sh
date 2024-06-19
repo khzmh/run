270c5c40c2839cefb1a23871f7bb65ab09e19e30
khzmh <sitikhojaimah@gmail.com>
2024-06-20 01:15:57 +0800

    Update reg.sh

--- reg.sh
+++ reg.sh
@@ -1,1 +1,1 @@
-gitlab-runner register --url https://gitlab-176132-0.cloudclusters.net/ --registration-token GR13489417vL16egf3xqR2BwU8map
+gitlab-runner register --url https://gitlab-176132-0.cloudclusters.net/ --registration-token GR1348941xpqgByPeos2LQzAukQ2X
